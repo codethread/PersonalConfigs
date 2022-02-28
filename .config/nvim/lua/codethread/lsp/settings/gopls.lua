5790866110cb820c32f7c22236cc894695a8de04
Adam Hall <adamhalldesigns@gmail.com>
2022-02-28 08:42:11 +0000

    update settings

---
+++ .config/nvim/lua/codethread/lsp/settings/gopls.lua
@@ -0,0 +1,9 @@
+return {
+	on_attach = function(client, bufnr)
+		-- turn off formatting as we'll use rustfmt
+		-- client.resolved_capabilities.document_formatting = false
+
+		require("codethread.lsp.settings.shared").lsp_highlight_document(client)
+		require("codethread.lsp.settings.shared").lsp_keymaps(bufnr)
+	end,
+}
--- .config/nvim/lua/codethread/settings.lua
+++ .config/nvim/lua/codethread/settings.lua
@@ -6,14 +6,11 @@ set.softtabstop = 2
 set.relativenumber = true
 
 set.hidden = true -- Required to keep multiple buffers open multiple buffers
--- set.nowrap = true -- Display long lines as just one line
 set.encoding = "utf-8" -- The encoding displayed
 set.pumheight = 10 -- Makes popup menu smaller
 set.fileencoding = "utf-8" -- The encoding written to file
 set.ruler = true -- Show the cursor position all the time
 set.cmdheight = 2 -- More space for displaying messages
--- set.iskeyword+=-                      	-- treat dash separated words as a word text object"
--- set.mouse=a                             -- Enable your mouse
 set.splitbelow = true -- Horizontal splits will automatically be below
 set.splitright = true -- Vertical splits will automatically be to the right
 set.conceallevel = 0 -- So that I can see `` in markdown files
@@ -28,25 +25,24 @@ set.number = true -- Line numbers
 set.cursorline = true -- Enable highlighting of the current line
 -- set.background=dark                     -- tell vim what the background color looks like
 set.showtabline = 2 -- Always show tabs
--- set.noshowmode = true -- We don't need to see things like -- INSERT -- anymore
--- set.nobackup = true -- This is recommended by coc
--- set.nowritebackup = true -- This is recommended by coc
 set.updatetime = 300 -- Faster completion
 set.timeoutlen = 500 -- By default timeoutlen is 1000 ms
 -- set.formatoptions-=cro                  -- Stop newline continution of comments
 set.clipboard:append("unnamedplus") -- Copy paste between vim and everything else
--- set.autochdir = true -- Your working directory will always be the same as your working directory
 set.signcolumn = "yes"
 set.completeopt = "menu,menuone,noselect"
 set.termguicolors = true -- adds more colors
-
--- set.foldmethod = "expr"
--- set.foldexpr = "nvim_treesitter#foldexpr()"
 
 vim.cmd([[
   set foldlevel=99
   set foldmethod=expr
   set foldexpr=nvim_treesitter#foldexpr()
+  set noshowmode " We don't need to see things like -- INSERT -- anymore
+  set nobackup " This is recommended by coc
+  set nowritebackup " This is recommended by coc
+  set nowrap " Display long lines as just one line
+  set iskeyword+=-                      	" treat dash separated words as a word text object"
+  set mouse=a                             " Enable your mouse
 ]])
 
 -- au! BufWritePost $MYVIMRC source %      " auto source when writing to init.vm alternatively you can run :source $MYVIMRC
