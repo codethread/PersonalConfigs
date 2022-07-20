2da98cf15e7bd27a864f99daf6baf6f0ecaa14c4
Adam Hall <adamhalldesigns@gmail.com>
2022-07-20 20:21:30 +0100

    neovim: lua lsp sumneko_lua with hopefully better defaults

--- .config/nvim/lua/codethread/lsp/settings/sumneko_lua.lua
+++ .config/nvim/lua/codethread/lsp/settings/sumneko_lua.lua
@@ -4,18 +4,18 @@ return {
 			runtime = {
 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
 				version = "LuaJIT",
-				-- Setup your lua path
-				path = vim.split(package.path, ";"),
 			},
 			diagnostics = {
+				-- Get the language server to recognize the `vim` global
 				globals = { "vim" },
 			},
 			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
 			},
 		},
 	},
