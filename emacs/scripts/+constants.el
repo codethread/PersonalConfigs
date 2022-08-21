51e27f0ce53ed2f01328768d4495e6916716bf44
Adam Hall <adamhalldesigns@gmail.com>
2022-08-21 20:22:26 +0100

    emacs: https://github.com/codethread/emacs.d

--- emacs/scripts/+constants.el
+++
@@ -1,9 +0,0 @@
-;;; +constants.el --- Shared constants needed for startup -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-(setq user-emacs-directory "~/.emacs.d")
-
-(provide '+constants)
-
-;;; +constants.el ends here
