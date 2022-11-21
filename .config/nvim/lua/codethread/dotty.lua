2684ae70739dd35d374a55c688ed560039f83e90
Adam Hall <adamhalldesigns@gmail.com>
2022-11-21 16:53:15 +0000

    neovim: fix dotty not running

--- .config/nvim/lua/codethread/dotty.lua
+++ .config/nvim/lua/codethread/dotty.lua
@@ -16,40 +16,34 @@ local dotty_setup_job = Job:new {
 	on_start = function() dotty_info 'Running' end,
 }
 
-local dotfiles = '*/PersonalConfigs/*'
-local group = vim.api.nvim_create_augroup('Dotty', { clear = false })
+vim.api.nvim_create_user_command('Dotty', function() dotty_setup_job:start() end, {})
 
 if vim.fn.executable 'dotty' then
-	vim.api.nvim_clear_autocmds { group = 'Dotty' }
-
-	-- run dotty when creating and deleting buffers
 	vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufFilePost', 'BufWipeout', 'VimLeavePre' }, {
-		group = group,
-		pattern = dotfiles,
+		desc = 'Run dotty when saving and deleting buffers',
+		group = vim.api.nvim_create_augroup('Dotty', {}),
+		pattern = '*/PersonalConfigs/*',
 		callback = function(opts)
 			if opts.file == nil then return end
 
-			Job:new({
+			local job = Job:new {
 				command = 'dotty',
 				args = { 'test', opts.file },
-				and_then_on_success = dotty_setup_job,
-			}):start()
+			}
+
+			job:and_then_on_success(dotty_setup_job)
+
+			job:start()
 		end,
 	})
 
 	-- handle nvim-tree which has it's own api
-	vim.api.nvim_create_autocmd('VimEnter', {
-		group = group,
-		pattern = dotfiles,
-		callback = function()
-			require('codethread.utils').safe_load('nvim-tree.api', function(api)
-				local events = { 'NodeRenamed', 'FileCreated', 'FileRemoved', 'FolderRemoved' }
-				for _, event in ipairs(events) do
-					api.events.subscribe(event, function() dotty_setup_job:start() end)
-				end
-			end)
-		end,
-	})
+	require('codethread.utils').safe_load('nvim-tree.api', function(api)
+		local events = { 'NodeRenamed', 'FileCreated', 'FileRemoved', 'FolderRemoved' }
+		for _, event in ipairs(events) do
+			api.events.subscribe(event, function() dotty_setup_job:start() end)
+		end
+	end)
 else
 	vim.notify('No Dotty bin, you may need to brew install', 'warn', {
 		title = 'Dotty',
