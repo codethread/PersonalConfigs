2f8f54bb218322a8920bd094089e6422e3438d9d
Adam Hall <adamhalldesigns@gmail.com>
2022-12-01 23:33:19 +0000

    neovim: fix gx for link

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -38,3 +38,28 @@ nt.setup {
 	--     nmap('<C-e>', '<cmd>NvimTreeFindFileToggle<cr>', { buffer = bufnr })
 	-- end
 }
+
+local function is_github_link(w)
+	local bits = vim.split(w, '/')
+	return #bits == 2
+end
+
+-- replace missing gx with netrw gone
+vim.keymap.set('n', 'gx', function()
+	local file = vim.fn.expand '<cfile>'
+	local Job = require 'plenary.job'
+
+	if vim.startswith(file, 'http') then
+		Job:new({
+			command = 'open',
+			args = { file },
+		}):sync()
+	elseif is_github_link(file) then
+		Job:new({
+			command = 'open',
+			args = { 'https://github.com/' .. file },
+		}):sync()
+	else
+		print('not a link: ' .. file)
+	end
+end, {})
