4b8267e3495eb0fef4b759f111a017f2b400850d
Adam Hall <adamhalldesigns@gmail.com>
2022-12-02 11:47:14 +0000

    neovim: better live commands

--- .config/nvim/after/plugin/live-command.lua
+++ .config/nvim/after/plugin/live-command.lua
@@ -1,5 +1,26 @@
 require('live-command').setup {
+	defaults = {
+		inline_highlighting = false,
+	},
+	break_undo = false,
 	commands = {
-		Norm = { cmd = 'norm' },
+		Norm = {
+			-- :%Norm 0f{ciwlook mum, no hands!
+			cmd = 'norm',
+		},
+		Reg = {
+			-- This will transform ":5Reg a" into ":norm 5@a", running a captured macro with a preview
+			cmd = 'norm',
+			args = function(opts) return (opts.count == -1 and '' or opts.count) .. '@' .. opts.args end,
+			range = '',
+		},
+		S = {
+			-- run :%/result/outcome and see bottome text change, using vim-abolish
+			cmd = 'Subvert',
+		},
 	},
 }
+
+-- result
+-- Result
+-- RESULT
