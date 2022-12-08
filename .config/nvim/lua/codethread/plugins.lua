daffb69533f35386ac80af29b7a3aa4d23b7acd7
Adam Hall <adamhalldesigns@gmail.com>
2022-12-08 10:07:57 +0000

    neovim: tidy plugins

--- .config/nvim/lua/codethread/plugins.lua
+++ .config/nvim/lua/codethread/plugins.lua
@@ -26,14 +26,58 @@ safe_load('packer', function(packer)
 			-- Things required early
 			use 'nvim-lua/plenary.nvim'
 			use 'nvim-lua/popup.nvim'
-			use 'tpope/vim-obsession'
-			use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdateSync' }
 			use 'kyazdani42/nvim-web-devicons'
+			use 'farmergreg/vim-lastplace'
 
-			-- misc
-			use { 'wakatime/vim-wakatime', tag = '9.*' }
+			-- treesitter
+			use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdateSync' }
+			use 'nvim-treesitter/nvim-treesitter-context'
+			use 'nvim-treesitter/nvim-treesitter-textobjects'
 			use 'nvim-treesitter/playground'
+			use 'windwp/nvim-ts-autotag' -- close <div tags, and ciw
+			use { 'JoosepAlviste/nvim-ts-context-commentstring', requires = 'tpope/vim-commentary' }
+			use { 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async', tag = 'v1.*' }
+
+			-- telescope
+			use {
+				'nvim-telescope/telescope.nvim',
+				tag = '0.1.*',
+				requires = { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+			}
+			use 'nvim-telescope/telescope-ui-select.nvim'
+
+			-- lore friendly improvements
+			use 'AckslD/nvim-neoclip.lua'
+			use 'ThePrimeagen/harpoon'
+			use 'famiu/bufdelete.nvim' -- delete buffer
+			use 'goolord/alpha-nvim'
+			use 'jghauser/mkdir.nvim'
 			use 'kevinhwang91/nvim-bqf' -- mainly just like 'o' to close lists
+			use 'mbbill/undotree'
+			use 'norcalli/nvim-colorizer.lua'
+			use 'szw/vim-maximizer'
+			use 'tpope/vim-eunuch' -- unix helpers, :Rename, :Delete
+			use 'tpope/vim-obsession'
+			use 'tpope/vim-projectionist'
+			use 'tpope/vim-rsi' -- readline movement, e.g C-f is forward char
+			use 'windwp/nvim-autopairs' -- Autopairs, integrates with both cmp and treesitter
+			use 'xorid/swap-split.nvim'
+			use { 'bennypowers/nvim-regexplainer', requires = 'MunifTanjim/nui.nvim' }
+			use { 'ellisonleao/glow.nvim', run = 'brew install glow' }
+			use { 'junegunn/vim-easy-align', tag = '2.*' }
+			use { 'kyazdani42/nvim-tree.lua', requires = 'gabebw/vim-github-link-opener' }
+			use { 'kylechui/nvim-surround', tag = 'v1.*' }
+			use { 'lukas-reineke/indent-blankline.nvim', tag = 'v2.*' }
+			use { 'phaazon/hop.nvim', tag = 'v2.*' }
+			use { 'rcarriga/nvim-notify', tag = 'v3.*' }
+			use { 'smjonas/live-command.nvim', tag = '1.*' } -- Text editing in Neovim with immediate visual feedback: view the effects of any command on your buffer contents live. Preview macros, the :norm command & more!
+			use { 'wakatime/vim-wakatime', tag = '9.*' }
+			use { 'windwp/nvim-spectre', run = 'brew install gnu-sed' } -- find/replace
+			-- string  Coercion
+			-- Want to turn fooBar into foo_bar? Press crs (coerce to snake_case). MixedCase
+			-- (crm), camelCase (crc), snake_case (crs), UPPER_CASE (cru), dash-case (cr-),
+			-- dot.case (cr.), space case (cr<space>), and Title Case (crt) are all just 3 keystrokes away.
+			use 'tpope/vim-abolish'
 
 			-- key bindings
 			use 'folke/which-key.nvim'
@@ -43,66 +87,7 @@ safe_load('packer', function(packer)
 			use { 'cormacrelf/dark-notify', run = 'brew install cormacrelf/tap/dark-notify' }
 			use 'shaunsingh/nord.nvim' -- classic, but very italic
 			use 'folke/tokyonight.nvim' -- snazzy and vibrant, works great with a background terminal image
-
-			-- modeline
 			use 'nvim-lualine/lualine.nvim'
-			-- project navigation
-			use 'farmergreg/vim-lastplace'
-			use 'goolord/alpha-nvim'
-			use {
-				'nvim-telescope/telescope.nvim',
-				tag = '0.1.*',
-				requires = { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
-			}
-
-			--[[
-        It sets vim.ui.select to telescope. That means for example that neovim core stuff
-        can fill the telescope picker. Example would be lua vim.lsp.buf.code_action().
-        --]]
-			use 'nvim-telescope/telescope-ui-select.nvim'
-
-			use 'tpope/vim-projectionist'
-
-			use {
-				'kyazdani42/nvim-tree.lua',
-
-				requires = 'gabebw/vim-github-link-opener',
-			}
-			-- use 'tpope/vim-vinegar' -- netrw improvements
-
-			use 'tpope/vim-eunuch' -- unix helpers, :Rename, :Delete
-
-			use 'ThePrimeagen/harpoon'
-
-			-- visual
-			-- show colors for css
-			use {
-				'norcalli/nvim-colorizer.lua',
-			}
-
-			use { 'bennypowers/nvim-regexplainer', requires = 'MunifTanjim/nui.nvim' }
-			use {
-				'ellisonleao/glow.nvim', --[[tag = '0.1.*',]]
-
-				run = 'brew install glow',
-			}
-			use { 'lukas-reineke/indent-blankline.nvim', tag = 'v2.*' }
-
-			-- project editing
-			use 'famiu/bufdelete.nvim' -- delete buffer
-			-- automatically creates missing folders
-			use 'jghauser/mkdir.nvim'
-			-- find/replace
-			use {
-				'windwp/nvim-spectre',
-
-				run = 'brew install gnu-sed',
-			}
-
-			-- file navigation
-			use 'nvim-treesitter/nvim-treesitter-context'
-			use 'SmiteshP/nvim-navic'
-			use { 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async', tag = 'v1.*' }
 
 			-- lsp
 			use {
@@ -110,16 +95,15 @@ safe_load('packer', function(packer)
 				requires = {
 					'williamboman/mason.nvim',
 					'williamboman/mason-lspconfig.nvim',
-					'jose-elias-alvarez/typescript.nvim',
 				},
 			}
-
-			use 'jose-elias-alvarez/null-ls.nvim'
+			use 'SmiteshP/nvim-navic'
 			use 'b0o/schemastore.nvim'
 			use 'gbrlsnchs/telescope-lsp-handlers.nvim'
-			use {
-				'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-			}
+			use 'jose-elias-alvarez/null-ls.nvim'
+			use 'simrat39/symbols-outline.nvim' -- TODO: didn't even realise i had this
+			use 'jose-elias-alvarez/typescript.nvim'
+			use 'https://git.sr.ht/~whynothugo/lsp_lines.nvim'
 
 			-- completion
 			use {
@@ -132,67 +116,14 @@ safe_load('packer', function(packer)
 					'onsails/lspkind.nvim',
 				},
 			}
-
 			use {
 				'saadparwaiz1/cmp_luasnip',
-				requires = {
-					'L3MON4D3/LuaSnip',
-					'rafamadriz/friendly-snippets',
-				},
+				requires = { 'L3MON4D3/LuaSnip', 'rafamadriz/friendly-snippets' },
 			}
-
-			-- windows
-			use 'szw/vim-maximizer'
-			use 'simrat39/symbols-outline.nvim'
-			use { 'rcarriga/nvim-notify', tag = 'v3.*' }
-			use 'xorid/swap-split.nvim'
-
-			-- editing
-			use 'tpope/vim-rsi' -- readline movement, e.g C-f is forward char
-			use {
-				'nvim-treesitter/nvim-treesitter-textobjects',
-			}
-
-			use 'windwp/nvim-ts-autotag' -- close <div tags, and ciw
-
-			--[[
-        Text editing in Neovim with immediate visual feedback: view the effects of any command on your buffer contents live. Preview macros, the :norm command & more!
-        --]]
-			use { 'smjonas/live-command.nvim', tag = '1.*' }
-
-			use { 'kylechui/nvim-surround', tag = 'v1.*' }
-			use 'windwp/nvim-autopairs' -- Autopairs, integrates with both cmp and treesitter
-			use 'mbbill/undotree'
-
-			use 'tpope/vim-abolish' -- string  Coercion
-			-- Want to turn fooBar into foo_bar? Press crs (coerce to snake_case). MixedCase
-			-- (crm), camelCase (crc), snake_case (crs), UPPER_CASE (cru), dash-case (cr-),
-			-- dot.case (cr.), space case (cr<space>), and Title Case (crt) are all just 3 keystrokes away.
-
-			use 'AckslD/nvim-neoclip.lua'
-
-			use {
-				'JoosepAlviste/nvim-ts-context-commentstring',
-
-				requires = 'tpope/vim-commentary',
-			} -- uses tree sitter for comment detection
-
-			use { 'junegunn/vim-easy-align', tag = '2.*' }
-			use { 'phaazon/hop.nvim', tag = 'v2.*' }
 
 			-- git
-			use {
-				'TimUntersberger/neogit',
-				requires = {
-					'sindrets/diffview.nvim',
-				},
-			}
+			use { 'TimUntersberger/neogit', requires = { 'sindrets/diffview.nvim' } }
 			use 'lewis6991/gitsigns.nvim'
-
-			use 'tpope/vim-fugitive'
-			-- use("tpope/vim-rhubarb") -- :GBrowse and other git things
-
-			-- langs
 
 			-- help for lua, TODO need to make this work
 			-- use 'wsdjeg/luarefvim'
@@ -203,7 +134,6 @@ safe_load('packer', function(packer)
 
 			-- terminal
 			use { 'akinsho/toggleterm.nvim', tag = 'v2.*' }
-
 			use 'christoomey/vim-tmux-navigator'
 
 			if PACKER_BOOTSTRAP then require('packer').sync() end
