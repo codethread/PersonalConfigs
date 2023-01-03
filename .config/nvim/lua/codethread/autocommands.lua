e1ac5d596684a88997e23413460814653c07f4c9
Adam Hall <adamhalldesigns@gmail.com>
2023-01-03 08:36:06 +0000

    nvim: add autocommand to focus on previous buffer

--- .config/nvim/lua/codethread/autocommands.lua
+++ .config/nvim/lua/codethread/autocommands.lua
@@ -31,3 +31,12 @@ vim.api.nvim_create_autocmd('FileType', {
 	pattern = 'help',
 	callback = function() vim.keymap.set('n', 'q', '<cmd>q<cr>', { silent = true, buffer = true }) end,
 })
+
+vim.api.nvim_create_autocmd('BufReadPost', {
+	desc = 'Open old buffers at last position',
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
+	end,
+})
