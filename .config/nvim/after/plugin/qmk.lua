0b1f9d57de954baedcf042ac667591ace246e546
adam.hall <adam.hall@perkbox.com>
2023-04-23 20:12:40 +0100

    nvim: qmk handle multiple keymaps

--- .config/nvim/after/plugin/qmk.lua
+++ .config/nvim/after/plugin/qmk.lua
@@ -2,13 +2,48 @@ local require = require('codethread.utils').require
 local qmk, ok = require 'qmk'
 if not ok then return end
 
-qmk.setup {
-	name = 'LAYOUT_preonic_grid',
-	layout = {
-		'_ x x x x x x _ x x x x x x',
-		'_ x x x x x x _ x x x x x x',
-		'_ x x x x x x _ x x x x x x',
-		'_ x x x x x x _ x x x x x x',
-		'_ x x x x x x _ x x x x x x',
-	},
-}
+local group = vim.api.nvim_create_augroup('MyQMK', {})
+
+vim.api.nvim_create_autocmd('BufEnter', {
+	desc = 'Setup preonic',
+	group = group,
+	pattern = '*keymap.c',
+	callback = function()
+		---@type qmk.UserConfig
+		local config = {
+			name = 'LAYOUT_preonic_grid',
+			comment_preview = {
+				keymap_overrides = {
+					_______ = '␣',
+				},
+			},
+			layout = {
+				'_ x x x x x x _ x x x x x x',
+				'_ x x x x x x _ x x x x x x',
+				'_ x x x x x x _ x x x x x x',
+				'_ x x x x x x _ x x x x x x',
+				'_ x x x x x x _ x x x x x x',
+			},
+		}
+		qmk.setup(config)
+	end,
+})
+
+vim.api.nvim_create_autocmd('BufEnter', {
+	desc = 'Setup alternate keymap',
+	group = group,
+	pattern = '*foo.keymap',
+	callback = function()
+		qmk.setup {
+			name = 'LAYOUT_preonic_grid',
+			variant = 'zmk',
+			layout = {
+				'x x x x x x x _ _ _ _ _ _ _ _ _ _ x x x x x x x',
+				'x x x x x x x _ _ _ _ _ _ _ _ _ _ x x x x x x x',
+				'x x x x x x x _ x x x _ _ x x x _ x x x x x x x',
+				'x x x x x x _ x x x x _ _ x x x x _ x x x x x x',
+				'x x x x x _ _ _ x x x _ _ x x x _ _ _ x x x x x',
+			},
+		}
+	end,
+})
