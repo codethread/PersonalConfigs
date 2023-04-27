f55845597e214eeff80534a17a8681255aab4428
adam.hall <adam.hall@perkbox.com>
2023-04-27 23:33:00 +0100

    nvim: fix dotty not working with PATH

--- .config/nvim/lua/dotty.lua
+++ .config/nvim/lua/dotty.lua
@@ -1,27 +1,28 @@
 local Job = require 'plenary.job'
 
-local in_dotfiles = vim.endswith(vim.fn.getcwd(), os.getenv 'DOTFILES')
-
----notify at info level
----@param msg string
-local function dotty_info(msg)
-	vim.notify(msg, 'info', {
-		title = 'Dotty',
-		hide_from_history = true,
-		timeout = 0,
-	})
-end
-
-local dotty_setup_job = Job:new {
-	command = 'dotty',
-	args = { 'setup' },
-	on_start = function() dotty_info 'Running' end,
-}
-
-vim.api.nvim_create_user_command('Dotty', function() dotty_setup_job:start() end, {})
+local in_dotfiles =
+	vim.endswith(vim.fn.getcwd(), os.getenv 'DOTFILES' or os.getenv 'HOME' .. '/.config')
 
 if in_dotfiles then
 	if vim.fn.executable 'dotty' then
+		---notify at info level
+		---@param msg string
+		local function dotty_info(msg)
+			vim.notify(msg, 1, {
+				title = 'Dotty',
+				hide_from_history = true,
+				timeout = 0,
+			})
+		end
+
+		local dotty_setup_job = Job:new {
+			command = 'dotty',
+			args = { 'setup' },
+			on_start = function() dotty_info 'Running' end,
+		}
+
+		vim.api.nvim_create_user_command('Dotty', function() dotty_setup_job:start() end, {})
+
 		vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufFilePost', 'BufWipeout', 'VimLeavePre' }, {
 			desc = 'Run dotty when saving and deleting buffers',
 			group = vim.api.nvim_create_augroup('Dotty', {}),
