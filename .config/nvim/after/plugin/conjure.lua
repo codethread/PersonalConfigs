707b4486ca344eaab3ccbb61bf66dccd33638175
adam.hall <adam.hall@perkbox.com>
2023-05-17 05:37:58 +0100

    nvim: no conjure in rust for now

--- .config/nvim/after/plugin/conjure.lua
+++ .config/nvim/after/plugin/conjure.lua
@@ -1,15 +1,6 @@
 vim.g['conjure#mapping#doc_word'] = false
 vim.g['conjure#extract#tree_sitter#enabled'] = true
-vim.g['conjure#filetypes'] = {
-	'clojure',
-	'fennel',
-	'janet',
-	'hy',
-	'julia',
-	'racket',
-	'scheme',
-	'lua',
-	'lisp',
-	'python',
-	-- 'rust',
-}
+vim.cmd [[
+let g:conjure#filetypes = [ 'clojure', 'fennel', 'janet', 'hy', 'julia', 'racket', 'scheme', 'lua', 'lisp', 'python' ]
+let g:conjure#filetype#rust = v:false
+]]
