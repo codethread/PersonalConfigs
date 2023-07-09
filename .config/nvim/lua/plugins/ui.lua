b82be360e5687eac8260baebc8ea17895e133e63
codethread <adamhalldesigns@gmail.com>
2023-07-09 22:47:05 +0100

    nvim: add dressing

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -184,10 +184,13 @@ return {
 	},
 
 	{
-		{
-			'goolord/alpha-nvim',
-			dependencies = { 'nvim-tree/nvim-web-devicons' },
-			config = function() require('alpha').setup(require('alpha.themes.startify').config) end,
-		},
+		'goolord/alpha-nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function() require('alpha').setup(require('alpha.themes.startify').config) end,
+	},
+
+	{
+		'stevearc/dressing.nvim',
+		opts = {},
 	},
 }
