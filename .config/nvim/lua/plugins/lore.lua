fc4f3cad20a3eb20fdf4fc6ffb795bebe4ad3c06
codethread <adamhalldesigns@gmail.com>
2023-09-11 10:06:41 +0100

    nvim: undotree mapping

--- .config/nvim/lua/plugins/lore.lua
+++ .config/nvim/lua/plugins/lore.lua
@@ -16,24 +16,25 @@ return {
 				inoremap { {<c-g>u
 			]]
 
+			vim.keymap.set('n', 'U', Cmd 'UndotreeToggle', {})
 			vim.opt.swapfile = false
 			vim.opt.backup = false
 			vim.opt.writebackup = false -- This is recommended by coc
 
 			vim.cmd [[
-        if has("persistent_undo")
-           let target_path = expand('~/.local/share/nvim/undodir')
+			if has("persistent_undo")
+			   let target_path = expand('~/.local/share/nvim/undodir')
 
-            " create the directory and any parent directories
-            " if the location does not exist.
-            if !isdirectory(target_path)
-                call mkdir(target_path, "p", 0700)
-            endif
+				" create the directory and any parent directories
+				" if the location does not exist.
+				if !isdirectory(target_path)
+					call mkdir(target_path, "p", 0700)
+				endif
 
-            let &undodir=target_path
-            set undofile
-        endif
-        ]]
+				let &undodir=target_path
+				set undofile
+			endif
+			]]
 		end,
 	},
 
