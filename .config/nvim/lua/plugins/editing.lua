0b63c1da1705219e9d448fc9c7b55d9fffd6afff
codethread <adamhalldesigns@gmail.com>
2024-03-20 06:15:46 +0000

    nvim: update qmk to add zmk dao

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -125,32 +125,61 @@ return {
 
 	{
 		'codethread/qmk.nvim',
-		ft = { 'c' },
-		config = function()
-			---@type qmk.UserConfig
-			local conf = {
-				name = 'LAYOUT_preonic_grid',
-				layout = {
-					'_ x x x x x x _ x x x x x x',
-					'_ x x x x x x _ x x x x x x',
-					'_ x x x x x x _ x x x x x x',
-					'_ x x x x x x _ x x x x x x',
-					'_ x x x x x x _ x x x x x x',
-				},
-				comment_preview = {
-					keymap_overrides = {
-						KC_LSFT = '󰘶',
-						KC_LALT = '⎇',
-						KC_LCTL = '^',
-						KC_LGUI = '󰘳',
-						LSFT_T = '󰘶',
-						LALT_T = '⎇',
-						LCTL_T = '^',
-						LGUI_T = '󰘳',
-					},
-				},
-			}
-			require('qmk').setup(conf)
+		init = function()
+			local group = vim.api.nvim_create_augroup('MyQMK', {})
+
+			vim.api.nvim_create_autocmd('BufEnter', {
+				desc = 'Format qmk keymap',
+				group = group,
+				pattern = '*/keymap.c',
+				callback = function()
+					---@type qmk.UserConfig
+					local conf = {
+						name = 'LAYOUT_preonic_grid',
+						layout = {
+							'_ x x x x x x _ x x x x x x',
+							'_ x x x x x x _ x x x x x x',
+							'_ x x x x x x _ x x x x x x',
+							'_ x x x x x x _ x x x x x x',
+							'_ x x x x x x _ x x x x x x',
+						},
+						comment_preview = {
+							keymap_overrides = {
+								KC_LSFT = '󰘶',
+								KC_LALT = '⎇',
+								KC_LCTL = '^',
+								KC_LGUI = '󰘳',
+								LSFT_T = '󰘶',
+								LALT_T = '⎇',
+								LCTL_T = '^',
+								LGUI_T = '󰘳',
+							},
+						},
+					}
+					require('qmk').setup(conf)
+				end,
+			})
+
+			vim.api.nvim_create_autocmd('BufEnter', {
+				desc = 'Format zmk keymap',
+				group = group,
+				pattern = '*/dao.keymap',
+				callback = function()
+					---@type qmk.UserConfig
+					local zmk_conf = {
+						name = 'hi',
+						variant = 'zmk',
+						auto_format_pattern = '*/dao.keymap',
+						layout = {
+							'_ x x x x x x _ x x x x x x',
+							'_ x x x x x x _ x x x x x x',
+							'_ x x x x x x _ x x x x x x',
+							'_ _ _ _ x x x _ x x x _ _ _',
+						},
+					}
+					require('qmk').setup(zmk_conf)
+				end,
+			})
 		end,
 	},
 
