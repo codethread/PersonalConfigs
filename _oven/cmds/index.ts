404caa37b1f00de5756b238e903df6d1742ab879
codethread <adamhalldesigns@gmail.com>
2024-10-29 19:03:48 +0000

    kindly: teams mr

--- _oven/cmds/index.ts
+++ _oven/cmds/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env bun
 
+import { parseArgs } from "util";
 import { bar, foo } from "lib";
 
 console.log(foo());
@@ -10,7 +11,24 @@ let cmds = [foo, bar];
 // @ts-ignore
 await import("@ct/work")
   .then((mods) => {
+    const {
+      values,
+      positionals: [title, url],
+    } = parseArgs({
+      args: Bun.argv.slice(2),
+      allowPositionals: true,
+    });
+
     // do whatever to add these commands to the global scope
+    if (mods.teamsStatus) {
+      // mods.postMr().then((res) => console.log(res));
+      mods
+        // .teamsStatus("back in a mo", { free: false, pinned: true })
+        // .teamsStatus("hi there", { free: true, pinned: false })
+        .teamsMr({ title, url })
+        .then((r) => console.log(r))
+        .catch((r) => console.log(r));
+    }
     console.log(mods);
     console.log(Object.values(mods));
     for (const mod of Object.values(mods)) {
@@ -21,4 +39,4 @@ await import("@ct/work")
     // ignore, not available
   });
 
-console.log(cmds.map((cmd) => cmd()));
+// console.log(cmds.map((cmd) => cmd()));
