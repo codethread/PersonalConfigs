96ad55c9ecdcaee54f4bea52e9d124d1225b889e
codethread <adamhalldesigns@gmail.com>
2024-11-01 19:22:58 +0000

    kindly: still playing

--- _oven/cmds/index.ts
+++ _oven/cmds/index.ts
@@ -8,35 +8,39 @@ console.log(bar());
 
 let cmds = [foo, bar];
 
+import { $ } from "bun";
+
+await $`aerospace resize smart -200`;
+
 // @ts-ignore
-await import("@ct/work")
-  .then((mods) => {
-    const {
-      values,
-      positionals: [title, url],
-    } = parseArgs({
-      args: Bun.argv.slice(2),
-      allowPositionals: true,
-    });
-
-    // do whatever to add these commands to the global scope
-    if (mods.teamsStatus) {
-      // mods.postMr().then((res) => console.log(res));
-      mods
-        // .teamsStatus("back in a mo", { free: false, pinned: true })
-        // .teamsStatus("hi there", { free: true, pinned: false })
-        .teamsMr({ title, url })
-        .then((r) => console.log(r))
-        .catch((r) => console.log(r));
-    }
-    console.log(mods);
-    console.log(Object.values(mods));
-    for (const mod of Object.values(mods)) {
-      cmds.push(mod);
-    }
-  })
-  .catch(() => {
-    // ignore, not available
-  });
-
-// console.log(cmds.map((cmd) => cmd()));
+// await import("@ct/work")
+//   .then((mods) => {
+//     const {
+//       values,
+//       positionals: [title, url],
+//     } = parseArgs({
+//       args: Bun.argv.slice(2),
+//       allowPositionals: true,
+//     });
+//
+//     // do whatever to add these commands to the global scope
+//     if (mods.teamsStatus) {
+//       // mods.postMr().then((res) => console.log(res));
+//       mods
+//         // .teamsStatus("back in a mo", { free: false, pinned: true })
+//         // .teamsStatus("hi there", { free: true, pinned: false })
+//         .teamsMr({ title, url })
+//         .then((r) => console.log(r))
+//         .catch((r) => console.log(r));
+//     }
+//     console.log(mods);
+//     console.log(Object.values(mods));
+//     for (const mod of Object.values(mods)) {
+//       cmds.push(mod);
+//     }
+//   })
+//   .catch(() => {
+//     // ignore, not available
+//   });
+//
+// // console.log(cmds.map((cmd) => cmd()));
