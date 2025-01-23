aef883515e94d74615fad29e9f632b7a5d698547
codethread <adamhalldesigns@gmail.com>
2025-01-23 15:39:58 +0000

    nvim: shrink bootstrap code

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,25 +14,18 @@ require 'codethread.config'
 -- disabled vim builtins
 require 'codethread.disabled'
 
-do -- 💤
-	-- Bootstrap lazy.nvim
+do -- 💤 bootstrap
 	local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-	---@diagnostic disable-next-line: undefined-field
-	if not (vim.uv or vim.loop).fs_stat(lazypath) then
-		local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-		local out =
-			vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-		if vim.v.shell_error ~= 0 then
-			vim.api.nvim_echo({
-				{ 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-				{ out, 'WarningMsg' },
-				{ '\nPress any key to exit...' },
-			}, true, {})
+	if not vim.uv.fs_stat(lazypath) then
+		--stylua: ignore
+		local out = vim.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', 'https://github.com/folke/lazy.nvim.git', lazypath, }) :wait()
+		if out.code ~= 0 then
+		--stylua: ignore
+			vim.api.nvim_echo({ { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' }, { out.stdout .. out.stderr, 'WarningMsg' }, { '\nPress any key to exit...' }, }, true, {})
 			vim.fn.getchar()
 			os.exit(1)
 		end
 	end
-	---@diagnostic disable-next-line: undefined-field
 	vim.opt.rtp:prepend(lazypath)
 end
 
