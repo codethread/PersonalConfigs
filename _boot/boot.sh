749df789821b0e4004c72c6837f4b3d25fab134d
Adam Hall <adamhalldesigns@gmail.com>
2025-08-04 14:31:14 +0100

    Update boot.sh

--- _boot/boot.sh
+++ _boot/boot.sh
@@ -36,7 +36,7 @@ echo "( ◕ ◡ ◕ ) Booting machine"
 echo "available again with 'boot machine --help'"
 
 export PATH="${HOME}/.local/bin:/opt/homebrew/bin:${PATH}"
-export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CONFIG_HOME="${DOTFILES}/.config"
 export XDG_DATA_HOME="${HOME}/.local/share"
 export XDG_STATE_HOME="${HOME}/.local/state"
 export XDG_CACHE_HOME="${HOME}/.local/cache"
