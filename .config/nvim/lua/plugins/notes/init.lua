8bcdf6fa458280eaa4837c70088401b70189d24e
codethread <adamhalldesigns@gmail.com>
2025-08-09 05:36:48 +0100

    nvim: images

--- .config/nvim/lua/plugins/notes/init.lua
+++ .config/nvim/lua/plugins/notes/init.lua
@@ -12,6 +12,19 @@ return {
 		'mzlogin/vim-markdown-toc',
 		init = function() vim.cmd [[let g:vmt_auto_update_on_save = 0]] end,
 	},
+	{
+		'3rd/image.nvim',
+		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+		opts = {
+			processor = 'magick_cli',
+			backend = 'kitty',
+			integrations = {
+				html = {
+					enabled = true,
+				},
+			},
+		},
+	},
 
 	{
 		'iamcco/markdown-preview.nvim',
