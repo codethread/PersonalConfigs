2844946a4e41a4f4277c2482f459874179e3f0d6
codethread <adamhalldesigns@gmail.com>
2026-01-06 20:13:09 +0000

    misc

--- config/nushell/scripts/ct/purge.nu
+++ config/nushell/scripts/ct/purge.nu
@@ -27,9 +27,33 @@ export def main [] {
 	let choice = (input $"(ansi red)PURGE?(ansi reset) [Y/n]: ")
 
 	if (($choice | str trim) != "n") {
+		let logfile = "/tmp/purge.log"
+		$"Purge started at (date now)\n" | save -f $logfile
+
 		$purged
 		| lines
-		| each {|dir| print $"cleaning $($dir)"; cd $dir | git clean -dfX }
+		| each {|dir|
+			$"Processing: ($dir)\n" | save -a $logfile
+			print $"(ansi cyan)Cleaning ($dir)(ansi reset)"
+
+			$"Running git clean...\n" | save -a $logfile
+			let result = do { git -C $dir clean -dfX } | complete
+			$"Exit code: ($result.exit_code)\n" | save -a $logfile
+			$"Stdout: ($result.stdout)\n" | save -a $logfile
+			$"Stderr: ($result.stderr)\n" | save -a $logfile
+
+			if ($result.exit_code != 0) {
+				print $"  (ansi red)Error:(ansi reset) not a git repository or git clean failed"
+			} else if ($result.stdout | str trim | is-empty) {
+				print $"  (ansi yellow)Nothing to remove(ansi reset)"
+			} else {
+				for line in ($result.stdout | lines) {
+					print $"  (ansi green)Removed:(ansi reset) ($line)"
+				}
+			}
+			$"Done with ($dir)\n" | save -a $logfile
+		}
+		$"Purge completed\n" | save -a $logfile
 	}
 
 }
--- config/nvim/lua/codethread/split.lua
+++ config/nvim/lua/codethread/split.lua
@@ -1,18 +1,58 @@
 local api = vim.api
 local M = {}
 
----Split the current window and open the previous buffer in left split
+---Split the current window and open the previous buffer in the new split
 ---handy when retrospectively wishing the file had opened in a split, e.g go-to-def
----
----For the future, I may want this to always use an existing pane, like a poor man's
----popout window
-function M.split()
+---@param direction? 'up' | 'left' | 'right' | 'down' | 'tab'
+function M.split(direction)
 	local ow = api.nvim_get_current_win()
-	vim.cmd.vsplit()
+	local is_split_down = vim.opt.splitbelow:get()
+	local is_split_right = vim.opt.splitright:get()
+
+	if direction == 'tab' then
+		vim.cmd 'tab split'
+		vim.cmd 'b#'
+		return
+	end
+
+	if direction == 'up' then
+		if is_split_down then
+			vim.opt.splitbelow = false
+			vim.cmd.split()
+			vim.opt.splitbelow = is_split_down
+		else
+			vim.cmd.split()
+		end
+	elseif direction == 'down' then
+		if not is_split_down then
+			vim.opt.splitbelow = true
+			vim.cmd.split()
+			vim.opt.splitbelow = is_split_down
+		else
+			vim.cmd.split()
+		end
+	elseif direction == 'left' then
+		if is_split_right then
+			vim.opt.splitright = false
+			vim.cmd.vsplit()
+			vim.opt.splitright = is_split_right
+		else
+			vim.cmd.vsplit()
+		end
+	else -- 'right' or nil (default)
+		if not is_split_right then
+			vim.opt.splitright = true
+			vim.cmd.vsplit()
+			vim.opt.splitright = is_split_right
+		else
+			vim.cmd.vsplit()
+		end
+	end
+
 	local nw = api.nvim_get_current_win()
-	vim.fn.win_gotoid(ow)
+	api.nvim_set_current_win(ow)
 	vim.cmd 'b#'
-	vim.fn.win_gotoid(nw)
+	api.nvim_set_current_win(nw)
 end
 
 return M
