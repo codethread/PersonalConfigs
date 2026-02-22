bad8f00c9ddef6df1058ca7314014c4e0a8bfa97
codethread <adamhalldesigns@gmail.com>
2026-02-22 21:40:13 +0000

    nixos: add LIBGL_ALWAYS_SOFTWARE for Electron apps in VM

--- nix/modules/nixos-desktop.nix
+++ nix/modules/nixos-desktop.nix
@@ -48,8 +48,9 @@
   # Propagate GTK theme into sessions
   environment.sessionVariables = {
     GTK_THEME = "rose-pine";
-    # VirtualBox / software-only GPU: wlroots has no hardware DRM to drive
-    WLR_NO_HARDWARE_CURSORS = "1";
-    WLR_RENDERER_ALLOW_SOFTWARE = "1";
+    # VirtualBox / software-only GPU
+    WLR_NO_HARDWARE_CURSORS = "1";     # wlroots: no hardware cursor plane
+    WLR_RENDERER_ALLOW_SOFTWARE = "1"; # wlroots: allow llvmpipe renderer
+    LIBGL_ALWAYS_SOFTWARE = "1";       # Electron/GL apps: skip GPU process entirely
   };
 }
