4d4476207f79a39f8c29c6bac93e607edc7430bd
codethread <adamhalldesigns@gmail.com>
2026-02-22 16:55:55 +0000

    nixos: clone PersonalConfigs and run boot machine on first activation

--- nix/modules/profiles/homelab.nix
+++ nix/modules/profiles/homelab.nix
@@ -7,6 +7,27 @@
 {
   home.stateVersion = "24.11";
 
+  # Clone PersonalConfigs and run dotfile setup on first activation.
+  # Subsequent activations are skipped (directory already exists).
+  home.activation.bootDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    DOTFILES="$HOME/PersonalConfigs"
+    if [ ! -d "$DOTFILES" ]; then
+      echo ">>> Cloning PersonalConfigs..."
+      ${pkgs.git}/bin/git clone --branch linux \
+        https://github.com/codethread/PersonalConfigs.git "$DOTFILES"
+      echo ">>> Running boot machine..."
+      export DOTFILES
+      export XDG_CONFIG_HOME="$DOTFILES/config"
+      export XDG_DATA_HOME="$HOME/.local/share"
+      export XDG_STATE_HOME="$HOME/.local/state"
+      export XDG_CACHE_HOME="$HOME/.local/cache"
+      ${pkgs.nushell}/bin/nu \
+        --env-config "$DOTFILES/config/nushell/env.nu" \
+        --config "$DOTFILES/config/nushell/config.nu" \
+        --commands "boot machine"
+    fi
+  '';
+
   home.packages = with pkgs; [
 
     # --- Editors / Terminal ---
